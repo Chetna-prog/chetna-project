032517e1c24eab53394ac25e9fd7bc73821d5810
Chetna-prog <74489353+Chetna-prog@users.noreply.github.com>
2020-11-17 06:16:37 +0530

    commit

--- db/vlog_db.sql
+++ db/vlog_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 17, 2020 at 01:16 AM
+-- Generation Time: Nov 17, 2020 at 01:45 AM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.1.28
 
